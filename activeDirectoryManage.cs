ad06f79ec6b1c130dbd9ce22d46dcc8cd0f96d58
PROGRAMACAO-002\Administrator <andre.fantinato@gmail.com>
2019-02-14 11:58:29 -0200

    primeiro commit

---
+++ activeDirectoryManage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.DirectoryServices.AccountManagement;
+
+public class activeDirectoryManage
+{
+    public bool addUser(string usuario, string senha)
+    {
+        using (var pc = new PrincipalContext(ContextType.Domain, "pathDomain", "container", "usernameAD", "passwordAD"))
+        {
+            using (var up = new UserPrincipal(pc))
+            {
+                try
+                {
+                    up.SamAccountName = usuario;
+                    //é necessário uma conexão ssl
+                    up.SetPassword(senha);
+                    //outras propriedades
+                    //up.Description = "";
+                    //up.DisplayName = "";
+                    //up.EmailAddress = "";
+                    up.Enabled = true;
+                    up.Save();
+
+                    return true;
+                }
+                catch (Exception)
+                {
+                    Console.Write("error");
+                }
+
+            }
+        }
+        return false;
+    }
+}
